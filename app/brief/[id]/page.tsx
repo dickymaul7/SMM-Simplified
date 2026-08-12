6db92b9a09f3b0ace29990191664cd0ec62f958e
dickymaul7 <dickymaul7@gmail.com>
2026-08-12 12:27:51 +0700

    Attach Brand Alignment QC to Full Brief

--- app/brief/[id]/page.tsx
+++ app/brief/[id]/page.tsx
@@ -1,7 +1,13 @@
+import BrandAlignmentPanel from "@/components/pages/brand-alignment-panel";
 import BriefClient from "@/components/pages/brief-client";
 
 export const dynamic = "force-dynamic";
 
 export default function BriefPage() {
-  return <BriefClient />;
+  return (
+    <>
+      <BriefClient />
+      <BrandAlignmentPanel />
+    </>
+  );
 }
