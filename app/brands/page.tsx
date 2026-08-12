1aaa654e563b950b79f75378757afbd10b0fbd50
dickymaul7 <dickymaul7@gmail.com>
2026-08-12 12:26:41 +0700

    Use Brand Intelligence workspace page

--- app/brands/page.tsx
+++ app/brands/page.tsx
@@ -1,7 +1,7 @@
-import WorkspacePlaceholder from "@/components/pages/workspace-placeholder";
+import BrandsClient from "@/components/pages/brands-client";
 
 export const dynamic = "force-dynamic";
 
 export default function BrandsPage() {
-  return <WorkspacePlaceholder kind="brands" />;
+  return <BrandsClient />;
 }
