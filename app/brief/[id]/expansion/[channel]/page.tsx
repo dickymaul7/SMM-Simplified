0bcafad8fb9582c5a87b13ecc156ad2cfedf9377
dickymaul7 <dickymaul7@gmail.com>
2026-08-13 11:23:20 +0700

    Add Content Expansion editor route

---
+++ app/brief/[id]/expansion/[channel]/page.tsx
@@ -0,0 +1,7 @@
+import ContentExpansionEditor from "@/components/pages/content-expansion-editor";
+
+export const dynamic = "force-dynamic";
+
+export default function ContentExpansionPage() {
+  return <ContentExpansionEditor />;
+}
