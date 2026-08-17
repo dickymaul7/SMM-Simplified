f800a8835ab9b31239a05dc15a0098c195fa4c07
dickymaul7 <dickymaul7@gmail.com>
2026-08-17 19:01:54 +0700

    Add calendar scheduling to content expansion page

--- app/brief/[id]/expansion/[channel]/page.tsx
+++ app/brief/[id]/expansion/[channel]/page.tsx
@@ -1,7 +1,13 @@
 import ContentExpansionEditor from "@/components/pages/content-expansion-editor";
+import ContentExpansionSchedulePanel from "@/components/pages/content-expansion-schedule-panel";
 
 export const dynamic = "force-dynamic";
 
 export default function ContentExpansionPage() {
-  return <ContentExpansionEditor />;
+  return (
+    <>
+      <ContentExpansionEditor />
+      <ContentExpansionSchedulePanel />
+    </>
+  );
 }
