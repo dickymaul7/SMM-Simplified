c23ea25698f25072af1acc064998dce2ccea68d7
dickymaul7 <dickymaul7@gmail.com>
2026-08-20 06:06:40 +0700

    feat: integrate task force into overview

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,7 +1,8 @@
 import OverviewClient from "@/components/pages/overview-client";
+import TaskForcePanel from "@/components/task-force-panel";
 
 export const dynamic = "force-dynamic";
 
 export default function OverviewPage() {
-  return <OverviewClient />;
+  return <><OverviewClient /><TaskForcePanel /></>;
 }
