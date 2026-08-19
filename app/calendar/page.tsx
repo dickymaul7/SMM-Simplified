b37d3534c3b80893996f2a68b170a5682bd24649
dickymaul7 <dickymaul7@gmail.com>
2026-08-20 06:06:58 +0700

    feat: integrate task assignment into calendar workspace

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,7 +1,8 @@
 import CalendarExpansionClient from "@/components/pages/calendar-expansion-client";
+import TaskAssignmentPanel from "@/components/task-assignment-panel";
 
 export const dynamic = "force-dynamic";
 
 export default function CalendarPage() {
-  return <CalendarExpansionClient />;
+  return <><CalendarExpansionClient /><div className="app-workspace mx-auto max-w-[1500px] px-4 pb-8 lg:px-6"><TaskAssignmentPanel /></div></>;
 }
