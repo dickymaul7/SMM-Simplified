af41125c939549e01da2262471c74c9d30d95d27
dickymaul7 <dickymaul7@gmail.com>
2026-08-26 16:01:30 +0700

    fix: render member task overview inside role-aware overview

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,13 +1,7 @@
 import RoleAwareOverview from "@/components/pages/role-aware-overview";
-import TaskForcePanel from "@/components/task-force-panel";
 
 export const dynamic = "force-dynamic";
 
 export default function OverviewPage() {
-  return (
-    <>
-      <RoleAwareOverview />
-      <TaskForcePanel />
-    </>
-  );
+  return <RoleAwareOverview />;
 }
